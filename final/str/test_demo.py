5edf789f0593797e58cf3ca76b0d1b390d31ac2a
arrowMBP <hanxinjiang980708@gmail.com>
2022-11-08 15:39:38 +0800

    test demo中新增了关于提取视频帧的处理方法，尝试将现在较慢的视频处理转换为图片处理

--- final/str/test_demo.py
+++ final/str/test_demo.py
@@ -1,6 +1,8 @@
 import cv2
 import face_recognition
 import numpy as np
+import os
+import threading
 
 
 # 好像是可以直接导入的
@@ -115,7 +117,39 @@ def multi_face_recognition():
 
     return 000
 
-def test_vscode_mbp():
-    print('hanxinjiang')
 
-test_vscode_mbp()
+def video_to_frames(video_path, outputDirName):
+    times = 0
+
+    #提取视频的帧率，每1帧提取一个
+    frame_frequency = 1
+
+    # if no path,create
+    if not os.path.exists(outputDirName):
+        os.makedirs(outputDirName)
+
+    # read frames
+    videos = cv2.VideoCapture(video_path)
+
+    while True:
+        times = times + 1
+        res, image = videos.read()
+        if not res:
+            print('not res, not image')
+            break
+        if times % frame_frequency == 0:
+            cv2.imwrite(outputDirName + '\\' + str(times) + '.jpg', image)
+    print('图片提取结束')
+    videos.release()
+
+
+if __name__ == '__main__':
+    input_dir = r'../document'
+    save_dir = r'../document'
+    count = 0
+    for video_name in os.listdir(input_dir):
+        video_path = os.path.join(input_dir, video_name)
+        outPutDirName = os.path.join(save_dir, video_name[:-4])
+        threading.Thread(target=video_to_frames, args=(video_path, outPutDirName)).start()
+        count = count + 1
+        print('%s th videos has been finished !!!' % count)
